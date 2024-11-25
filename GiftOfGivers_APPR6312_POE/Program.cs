fbaa7f4ee916dc119af04071374d5c6b97b8a714
Ntwalo <167986997+Ntwalo@users.noreply.github.com>
2024-11-25 20:00:05 +0200

    Update Program.cs

--- GiftOfGivers_APPR6312_POE/Program.cs
+++ GiftOfGivers_APPR6312_POE/Program.cs
@@ -1,32 +1,37 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using GiftOfGivers_APPR6312_POE.Data;
+using GiftOfGivers_APPR6312_POE.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<IDisasterRepository, DisasterRepository>();
-builder.Services.AddScoped<IDonationItemRepository, DonationItemRepository>();
-builder.Services.AddScoped<IDonationMoneyRepository, DonationMoneyRepository>();
-builder.Services.AddScoped<ICatagoryRepository, CatagoryRepository>();  
-builder.Services.AddScoped<IApplicationUserRepository, ApplicationUserRepository>();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddDefaultTokenProviders()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Register your custom services
+builder.Services.AddScoped<DisasterDataService>();
+
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-//if (args.Length == 1 && args[0].ToLower() == "seeddata")
-//{
-//    //Seed.SeedUsersAndRolesAsync(app);
-//    Seed.SeedData(app);
-//}
-
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -35,10 +40,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
